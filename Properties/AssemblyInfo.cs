f00730b3a8f13b69cf1c2ab4cabcd445a1db910c
ParallaxIT <rsutherland@thedrillbook.com>
2014-03-27 11:01:48 -0600

    jQuery

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AIS_Time")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("TimeTracker Program")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Athena Industrial")]
 [assembly: AssemblyProduct("AIS_Time")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
